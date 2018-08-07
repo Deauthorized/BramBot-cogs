9f166dc718cb1fb1e822f8006769a637d0558dbb
Brramble <Brramble@users.noreply.github.com>
2018-08-07 12:04:16 +0100

    Add files via upload

---
+++ avatar/avatar.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+from redbot.core import checks
+
+class Avatar:
+	"""View a user's avatar"""
+
+	def __init__(self,bot):
+		self.bot = bot
+    
+	@commands.command(pass_context=True)
+	async def avatar(self, ctx, user: discord.Member=None):
+		"""Embed profile image!"""
+		author = ctx.message.author
+		em = discord.Embed(colour=author.colour)
+		if not user:
+			user = author
+		if user.avatar_url:
+			em.set_image(url=user.avatar_url)
+		else:
+			em.set_image(url=user.default_avatar_url)
+		await ctx.send(embed=em)
