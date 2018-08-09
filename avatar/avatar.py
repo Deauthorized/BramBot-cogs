3fdf941ad801e337adad1270e1dfea67cd4f5789
Brramble <Brramble@users.noreply.github.com>
2018-08-09 09:53:54 +0100

    support for set embed colour

--- avatar/avatar.py
+++ avatar/avatar.py
@@ -12,11 +12,14 @@ class Avatar:
 	async def avatar(self, ctx, user: discord.Member=None):
 		"""Embed profile image!"""
 		author = ctx.message.author
-		em = discord.Embed(colour=author.colour)
+		em = discord.Embed()
 		if not user:
 			user = author
 		if user.avatar_url:
 			em.set_image(url=user.avatar_url)
 		else:
 			em.set_image(url=user.default_avatar_url)
-		await ctx.send(embed=em)
+		if await ctx.embed_requested():
+            		await ctx.send(embed=em)
+		else:
+			await ctx.send(embed=em)
