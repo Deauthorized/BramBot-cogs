f3c4be2fb03515facf99be53d2b2016d067af9b6
Brramble <Brramble@users.noreply.github.com>
2018-08-12 02:41:45 +0100

    Update pinginfo.py

--- ping/pinginfo.py
+++ ping/pinginfo.py
@@ -17,3 +17,4 @@ class Pinginfo:
         for shard, pingt in latencies:
             msg += "Shard {}/{}: {}ms\n".format(shard + 1, len(latencies), round(pingt*1000))
         em = discord.Embed(title=msg, colour=(await ctx.embed_colour()))
+        await ctx.send(embed=em)
