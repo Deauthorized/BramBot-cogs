4e526d75f88f43d08fe378f3958f508ecdf58d74
Brramble <Brramble@users.noreply.github.com>
2018-08-12 02:29:07 +0100

    Update avatar.py

--- avatar/avatar.py
+++ avatar/avatar.py
@@ -12,12 +12,11 @@ class Avatar:
 	async def avatar(self, ctx, user: discord.Member=None):
 		"""Embed profile image!"""
 		author = ctx.message.author
-		em = discord.Embed()
+		em = discord.Embed(colour=(await ctx.embed_colour()))
 		if not user:
 			user = author
 		if user.avatar_url:
 			em.set_image(url=user.avatar_url)
 		else:
 			em.set_image(url=user.default_avatar_url)
-		await ctx.embed_colour()
 		await ctx.send(embed=em)
