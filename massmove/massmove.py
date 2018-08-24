261eb97924e0fb53601e36f5542b237a9108f2c0
Brramble <Brramble@users.noreply.github.com>
2018-08-24 23:46:51 +0100

    formatting

--- massmove/massmove.py
+++ massmove/massmove.py
@@ -14,24 +14,30 @@ class Massmove:
 
     @commands.command(pass_context=True)
     @checks.admin_or_permissions(move_members=True)
-    async def massmove(self, ctx, from_channel: discord.VoiceChannel, to_channel: discord.VoiceChannel):
+    async def massmove(
+        self, ctx, from_channel: discord.VoiceChannel, to_channel: discord.VoiceChannel
+    ):
         """Massmove users to another voice channel. Use voice channel ID's."""
         await self._massmove(ctx, from_channel, to_channel)
 
     async def _massmove(self, ctx, from_channel, to_channel):
         """Internal function: Massmove users to another voice channel"""
         try:
-            print('Starting move on SID: {}'.format(from_channel.guild.id))
-            print('Getting copy of current list to move')
-            em = discord.Embed(title="Successfully moved users from **{}** to **{}**".format(from_channel.name, to_channel.name), 
-                colour=(await ctx.embed_colour()))
+            print("Starting move on SID: {}".format(from_channel.guild.id))
+            print("Getting copy of current list to move")
+            em = discord.Embed(
+                title="Successfully moved users from **{}** to **{}**".format(
+                    from_channel.name, to_channel.name
+                ),
+                colour=(await ctx.embed_colour()),
+            )
             await ctx.send(embed=em)
             voice_list = list(from_channel.members)
             for member in voice_list:
                 await member.move_to(to_channel)
-                print('Member {} moved to channel {}'.format(member.id, to_channel.id))
+                print("Member {} moved to channel {}".format(member.id, to_channel.id))
                 await asyncio.sleep(0.05)
         except discord.Forbidden:
-            await ctx.send('I have no permission to move members.')
+            await ctx.send("I have no permission to move members.")
         except discord.HTTPException:
-            await ctx.send('A error occured. Please try again')
+            await ctx.send("A error occured. Please try again")
